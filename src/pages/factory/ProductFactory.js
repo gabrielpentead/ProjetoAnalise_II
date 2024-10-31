57781346a527e0a31da37bdd701a99dbf16db6e6
gabrielpentead <gabrielpentead@gmail.com>
2024-10-31 12:02:46 -0300

    first commit

--- src/pages/factory/ProductFactory.js
+++ src/pages/factory/ProductFactory.js
@@ -1,7 +1,14 @@
 import { data } from '../../data';
 
 class ProductFactory {
-    static createProductByType(type) {
+    constructor() {
+        if (!ProductFactory.instance) {
+            ProductFactory.instance = this;
+        }
+        return ProductFactory.instance;
+    }
+
+    createProductByType(type) {
         const validTypes = ['fruta', 'verdura', 'legume', 'hortalica', 'outro'];
         if (!validTypes.includes(type)) {
             return []; // Retorna um array vazio se o tipo não for reconhecido
@@ -10,4 +17,8 @@ class ProductFactory {
     }
 }
 
-export default ProductFactory;
+// Congela a instância para evitar modificações
+const instance = new ProductFactory();
+Object.freeze(instance);
+
+export default instance;
